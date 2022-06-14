66f321af3bc19c4cf60b756038c5be826bb06ea6
Chadd <chadd_artaud@cox.net>
2022-06-14 08:53:47 -0700

    created routes to preform CRUD operations

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: ProductTag}]
+    });
+    res.status(200).json(productData);
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // create new product
@@ -91,6 +105,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const userData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(userData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
